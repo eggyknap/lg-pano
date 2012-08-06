91737ab38937921aa9397c1f0d823108f0108247
Joshua Tolley <josh@endpoint.com>
2012-08-06 10:43:02 -0600

    Forgot to indend read-event.c

--- read-event.c
+++ read-event.c
@@ -22,75 +22,77 @@
 
 int spacenav_fd;
 
-int init_spacenav(const char *dev_name) {
-    if ((spacenav_fd = open(dev_name, O_RDONLY | O_NONBLOCK)) < 0) {
-        perror("Unable to open spacenav input device");
-        return 0;
-    }
-    return 1;
+int init_spacenav(const char *dev_name)
+{
+	if ((spacenav_fd = open(dev_name, O_RDONLY | O_NONBLOCK)) < 0) {
+		perror("Unable to open spacenav input device");
+		return 0;
+	}
+	return 1;
 }
 
-int get_spacenav_event(spnav_event *p) {
-    int x, y, z, yaw, pitch, roll;
-    x = y = z = yaw = pitch = roll = 0;
-    struct input_event ev;
-    struct input_event *event_data = &ev;
+int get_spacenav_event(spnav_event * p)
+{
+	int x, y, z, yaw, pitch, roll;
+	x = y = z = yaw = pitch = roll = 0;
+	struct input_event ev;
+	struct input_event *event_data = &ev;
 
-    int num_read = read(spacenav_fd, event_data, sizeof(ev));
+	int num_read = read(spacenav_fd, event_data, sizeof(ev));
 
-    if (sizeof(ev) != num_read) {
-        return 0;
-    }
+	if (sizeof(ev) != num_read) {
+		return 0;
+	}
 
-    if (event_data->type == EV_KEY) {
-        fprintf(stderr, "button press\n");
-        return 1;
-    } else if (event_data->type == EV_SYN) {
-    // EV_SYN type may be quite useful for some devices
-    // identifies the sent data complete and therefore apply-able
+	if (event_data->type == EV_KEY) {
+		fprintf(stderr, "button press\n");
+		return 1;
+	} else if (event_data->type == EV_SYN) {
+		// EV_SYN type may be quite useful for some devices
+		// identifies the sent data complete and therefore apply-able
 //      fprintf(stderr, "sync event\n");
-        return 1;
-    } else if (event_data->type == EV_REL || event_data->type == EV_ABS) {
-        int axis = event_data->code;
-        int amount = event_data->value;
+		return 1;
+	} else if (event_data->type == EV_REL || event_data->type == EV_ABS) {
+		int axis = event_data->code;
+		int amount = event_data->value;
 
-        switch(axis) {
-            case 0:
-                x = amount;
-                break;
-            case 1:
-                y = amount;
-                break;
-            case 2:
-                z = amount;
-                break;
-            case 3:
-                pitch = amount;
-                break;
-            case 4:
-                roll = amount;
-                break;
-            case 5:
-                yaw = amount;
-                break;
-            default:
-                fprintf(stderr, "unknown axis event\n");
-                break;
-        }
-        p->motion.x = x;
-        p->motion.y = y;
-        p->motion.z = z;
-        p->motion.yaw = yaw;
-        p->motion.pitch = pitch;
-        p->motion.roll = roll;
+		switch (axis) {
+		case 0:
+			x = amount;
+			break;
+		case 1:
+			y = amount;
+			break;
+		case 2:
+			z = amount;
+			break;
+		case 3:
+			pitch = amount;
+			break;
+		case 4:
+			roll = amount;
+			break;
+		case 5:
+			yaw = amount;
+			break;
+		default:
+			fprintf(stderr, "unknown axis event\n");
+			break;
+		}
+		p->motion.x = x;
+		p->motion.y = y;
+		p->motion.z = z;
+		p->motion.yaw = yaw;
+		p->motion.pitch = pitch;
+		p->motion.roll = roll;
 
-        return 1;
+		return 1;
 
-    } else {
-        int evtype = event_data->type;
+	} else {
+		int evtype = event_data->type;
 
-        fprintf(stderr, "Unknown event type \"%d\".\n", evtype);
-        return 0;
-    }
-    return 1;
+		fprintf(stderr, "Unknown event type \"%d\".\n", evtype);
+		return 0;
+	}
+	return 1;
 }
