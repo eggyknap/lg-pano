891ef3c29190fab7a4a5a7584c48d9ea1a096789
Joshua Tolley <josh@endpoint.com>
2012-11-02 18:52:51 -0600

    Remove lots of unnecessary comments

--- lg-pano.c
+++ lg-pano.c
@@ -822,16 +822,8 @@ int setup_listen_port(void) {
 int main(int argc, char * argv[]) {
     /* XXX Copy lg-xiv options, where needed */
     const SDL_VideoInfo* info = NULL;
-
-    /* BEGIN */
-    /* Information about the current video settings. */
-    /* Dimensions of our window. */
-    /* Color depth in bits of our window. */
     int bpp = 0;
-    /* Flags we will pass into SDL_SetVideoMode. */
     int flags = 0;
-
-    /* END */
 
     spnav_event spev;
 
@@ -843,16 +835,12 @@ int main(int argc, char * argv[]) {
 
     get_options(argc, argv);
 
-    /* SDL BEGIN */
-    /* First, initialize SDL's video subsystem. */
     if( SDL_Init( SDL_INIT_VIDEO ) < 0 ) {
-        /* Failed, exit. */
         fprintf( stderr, "Video initialization failed: %s\n",
              SDL_GetError( ) );
         exit( 1 );
     }
 
-    /* Let's get some video information. */
     info = SDL_GetVideoInfo( );
 
     if( !info ) {
@@ -862,27 +850,10 @@ int main(int argc, char * argv[]) {
         exit( 1 );
     }
 
-    /* XXX Set our width/height to 640/480 (you would of course let the user
-     * decide this in a normal app). We get the bpp we will request from the
-     * display. On X11, VidMode can't change resolution, so this is probably being
-     * overly safe. Under Win32, ChangeDisplaySettings can change the bpp.  */
-
     screen_width = info->current_w;
     screen_height = info->current_h;
 
     bpp = info->vfmt->BitsPerPixel;
-
-    /*
-     * Now, we want to setup our requested window attributes for our OpenGL
-     * window.  We want *at least* 5 bits of red, green and blue. We also want
-     * at least a 16-bit depth buffer.
-     *
-     * The last thing we do is request a double buffered window. '1' turns on
-     * double buffering, '0' turns it off.
-     *
-     * Note that we do not use SDL_DOUBLEBUF in the flags to SDL_SetVideoMode.
-     * That does not affect the GL attribute state, only the standard 2D
-     * blitting setup.  */
 
     SDL_GL_SetAttribute( SDL_GL_RED_SIZE, 5 );
     SDL_GL_SetAttribute( SDL_GL_GREEN_SIZE, 5 );
@@ -890,34 +861,15 @@ int main(int argc, char * argv[]) {
     SDL_GL_SetAttribute( SDL_GL_DEPTH_SIZE, 16 );
     SDL_GL_SetAttribute( SDL_GL_DOUBLEBUFFER, 1 );
 
-    /*
-     * We want to request that SDL provide us with an OpenGL window, in a
-     * fullscreen video mode.
-     *
-     * EXERCISE:
-     * Make starting windowed an option, and handle the resize events properly
-     * with glViewport.  */
-
     flags = SDL_OPENGL;
     if (options.fullscreen)
         flags |= SDL_FULLSCREEN;
 
-    /*
-     * Set the video mode
-     */
     if( SDL_SetVideoMode( screen_width, screen_height, bpp, flags ) == 0 ) {
-        /* 
-         * This could happen for a variety of reasons,
-         * including DISPLAY not being set, the specified
-         * resolution not being available, etc.  */
-
         fprintf( stderr, "Video mode set failed: %s\n",
              SDL_GetError( ) );
         exit(1);
     }
-    /* SDL END */
-
-    /* XXX pay attention to options.fullscreen */
 
     glClearColor(1.0f, 1.0f, 1.0f, 1.0f);
 
